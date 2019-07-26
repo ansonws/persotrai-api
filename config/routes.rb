ffd14b079464dc5e0c2fa5af8b77b2e662b713ee
Kota Watanabe <kota3316@gmail.com>
2019-07-26 15:01:36 -0700

    setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :trainers do
+        resources :reviews, only: [:create, :destroy]
+        resources :favourites, only: [:create, :destroy]
+        member do 
+          get :trainer_dashboard
+        end
+      end
+      resource :session, only: [:create, :destroy]
+      resources :users do
+        get :current, on: :collection
+        member do 
+          get :change_password
+          patch :update_password
+          get :dashboard
+        end
+      end
+      resources :lessons,only:[:create, :destroy, :show, :edit, :update] do
+        member do
+          put :request
+          put :approve
+          put :decline
+          put :pay
+          put :finish
+          put :done
+        end
+    end
+  end
 end
