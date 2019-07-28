6db71f26d58afcb27125392c7dc1b022b444d2cf
Anson Lam <anson.lam94@gmail.com>
2019-07-28 16:49:57 -0700

    Fix unexpected end in favourites controller

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::FavouritesController < Api::ApplicationController
         favourite = Favourite.new user: current_user, trainer: trainer
         favourite.save!
         render json: { status:201, id: favourite.id }
-        end
       end
 
       def destroy
